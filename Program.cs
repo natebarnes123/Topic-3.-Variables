ee456a03fc606f3cda1b95534f8be109143e35a9
Nathan Barnes <339723215@GoTVDSB.ca>
2024-09-16 09:39:20 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+
+namespace Topic_3._Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int roomNumber = 29;
+            double price = 2.99;
+            string learning = "learning a bit about computer science!";
+
+
+            Console.WriteLine("This is room #" + roomNumber);
+            Console.WriteLine("The price is " + price);
+            Console.WriteLine("I am " + learning);
+
+            string name = "Nate Barnes";
+            int gradYear = 2026;
+
+            Console.WriteLine();
+            Console.WriteLine("My name is " + name + " and I'll graduate in " + gradYear + ".");
+            Console.WriteLine();
+            int radius = 2;
+            double area = 12.57;
+            Console.WriteLine("Area of a circle is the radius squared times PI.");
+            Console.WriteLine("The area of a circle with a radius of 2 is" + (area = Math.PI * (radius * 2));
+
+
+
+
+
+
+
+
+        }
+    }
+}
