46250010fe4ced83dffa341929b04c3accc1b806
Nathan Barnes <339723215@GoTVDSB.ca>
2024-09-17 09:02:21 -0400

    Finished topic 3.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Diagnostics.Tracing;
+using System.Reflection;
 
 namespace Topic_3._Variables
 {
@@ -24,11 +25,14 @@ namespace Topic_3._Variables
             int radius = 2;
             double area = 12.57;
             Console.WriteLine("Area of a circle is the radius squared times PI.");
-            Console.WriteLine("The area of a circle with a radius of 2 is" + (area = Math.PI * (radius * 2);
+            Console.WriteLine("The area of a circle with a radius of 2 is " + (area = Math.PI) * radius * 2);
+            Console.WriteLine();
+            //Finding the area of a sphere.
+            int diameter = 4;
+            double PI = 3.14159265359;
 
-
-
-
+            Console.WriteLine("To find the area of a sphere, the formula is 4 * PI * diameter");
+            Console.WriteLine("The area of a sphere with a diameter of four is " + 4 * PI * 4);
 
 
 
